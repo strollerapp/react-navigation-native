6f357a94a99332b7a7b35b76e798c363cd93edac
tidemandholger <holger.tidemand@stroller.life>
2020-10-05 09:29:55 +0200

    Apply fix for error: currentlyFocusedField is deprecated and will be removed in a future release. Use currentlyFocusedInput

--- src/createKeyboardAwareNavigator.js
+++ src/createKeyboardAwareNavigator.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import { TextInput } from 'react-native';
+import React from 'react'
+import { TextInput } from 'react-native'
 
 export default (Navigator, navigatorConfig) =>
   class KeyboardAwareNavigator extends React.Component {
-    static router = Navigator.router;
-    static navigationOptions = Navigator.navigationOptions;
-    _previouslyFocusedTextInput = null;
+    static router = Navigator.router
+    static navigationOptions = Navigator.navigationOptions
+    _previouslyFocusedTextInput = null
 
     _handleGestureBegin = () => {
-      this._previouslyFocusedTextInput = TextInput.State.currentlyFocusedField();
+      this._previouslyFocusedTextInput = TextInput.State.currentlyFocusedInput
+        ? TextInput.State.currentlyFocusedInput()
+        : TextInput.State.currentlyFocusedField()
       if (this._previouslyFocusedTextInput) {
-        TextInput.State.blurTextInput(this._previouslyFocusedTextInput);
+        TextInput.State.blurTextInput(this._previouslyFocusedTextInput)
       }
-      this.props.onGestureBegin && this.props.onGestureBegin();
-    };
+      this.props.onGestureBegin && this.props.onGestureBegin()
+    }
 
     _handleGestureCanceled = () => {
       if (this._previouslyFocusedTextInput) {
-        TextInput.State.focusTextInput(this._previouslyFocusedTextInput);
+        TextInput.State.focusTextInput(this._previouslyFocusedTextInput)
       }
-      this.props.onGestureCanceled && this.props.onGestureCanceled();
-    };
+      this.props.onGestureCanceled && this.props.onGestureCanceled()
+    }
 
     _handleGestureEnd = () => {
-      this._previouslyFocusedTextInput = null;
-      this.props.onGestureFinish && this.props.onGestureFinish();
-    };
+      this._previouslyFocusedTextInput = null
+      this.props.onGestureFinish && this.props.onGestureFinish()
+    }
 
     _handleTransitionStart = (transitionProps, prevTransitionProps) => {
       // TODO: We should not even have received the transition start event
       // in the case where the index did not change, I believe. We
       // should revisit this after 2.0 release.
       if (transitionProps.index !== prevTransitionProps.index) {
-        const currentField = TextInput.State.currentlyFocusedField();
+        const currentField = TextInput.State.currentlyFocusedInput
+          ? TextInput.State.currentlyFocusedInput()
+          : TextInput.State.currentlyFocusedField()
         if (currentField) {
-          TextInput.State.blurTextInput(currentField);
+          TextInput.State.blurTextInput(currentField)
         }
       }
 
       const onTransitionStart =
-        this.props.onTransitionStart || navigatorConfig.onTransitionStart;
+        this.props.onTransitionStart || navigatorConfig.onTransitionStart
       onTransitionStart &&
-        onTransitionStart(transitionProps, prevTransitionProps);
-    };
+        onTransitionStart(transitionProps, prevTransitionProps)
+    }
 
     render() {
       return (
@@ -53,6 +57,6 @@ export default (Navigator, navigatorConfig) =>
           onGestureEnd={this._handleGestureEnd}
           onTransitionStart={this._handleTransitionStart}
         />
-      );
+      )
     }
-  };
+  }
